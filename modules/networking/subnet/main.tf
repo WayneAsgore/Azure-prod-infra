4d13de3072be7ee43989f30f9c49420ac35fd4cf
Miklos Gal <galmiklos691@gmail.com>
2025-06-23 19:43:18 +0200

    test 9.1

--- modules/networking/subnet/main.tf
+++ modules/networking/subnet/main.tf
@@ -3,5 +3,5 @@ resource "azurerm_subnet" "sub" {
   resource_group_name = var.rg_name
   virtual_network_name = var.virtual_network_name
   address_prefixes = var.subnet_ap
-  depends_on       = var.virtual_network_id
+  depends_on       = [module.vnet]
 }
